3d8a01834c1b3b93390db59d60fe1f3a09ad506b
quangtn266 <quangtrandn93@gmail.com>
2024-02-02 20:39:17 +0700

    Update udf's source code

--- imooc-flink-project/src/main/java/com/imooc/flink/app/FlinkETLApp.java
+++ imooc-flink-project/src/main/java/com/imooc/flink/app/FlinkETLApp.java
@@ -1,4 +1,80 @@
 package com.imooc.flink.app;
 
+import com.alibaba.fastjson.JSON;
+import com.imooc.flink.domain.Access;
+import com.imooc.flink.domain.AccessV2;
+import com.imooc.flink.kafka.FlinkUtils;
+import com.imooc.flink.kafka.PKKafkaDeserializationSchema;
+import com.imooc.flink.udf.GaodeLocationMapFunction;
+import org.apache.commons.lang3.time.FastDateFormat;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.streaming.api.datastream.DataStream;
+
 public class FlinkETLApp {
+
+    public static void main(String[] args) throws Exception {
+        DataStream<Tuple2<String, String>> source = FlinkUtils.createKafkaStreamV3(args, PKKafkaDeserializationSchema.class);
+
+        FastDateFormat format = FastDateFormat.getInstance("yyyyMMdd-HH");
+
+        source.map(new MapFunction<Tuple2<String, String>, AccessV2>() {
+            @Override
+            public AccessV2 map(Tuple2<String, String> value) throws Exception {
+                try {
+
+                    AccessV2 bean = JSON.parseObject(value.f1, AccessV2.class);
+                    been.id = value.f0;
+
+                    long time = bean.time;
+                    String[] splits = format.format(time).split("-");
+                    String day = splits[0];
+                    String hour = splits[1];
+                    bean.day = day;
+                    bean.hour = hour;
+
+                    return bean;
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }).filter(x -> x != null)
+                .filter(new FilterFunction<AccessV2>() {
+                    @Override
+                    public boolean filter(AccessV2 value) throws Exception {
+                        return "startup".equals(value.event);
+                    }
+                }).addSink(JdbcSink.sink(
+                        "insert into ch_event values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                        (pstmt, x) -> {
+                            pstmt.setString(1, x.id);
+                            pstmt.setString(2, x.device);
+                            pstmt.setString(3, x.deviceType);
+                            pstmt.setString(4, x.os);
+                            pstmt.setString(5, x.event);
+                            pstmt.setString(6, x.net);
+                            pstmt.setString(7, x.channel);
+                            pstmt.setString(8, x.uid);
+                            pstmt.setInt(9, x.nu);
+                            pstmt.setString(10, x.ip);
+                            pstmt.setLong(11, x.time);
+                            pstmt.setString(12, x.version);
+                            pstmt.setString(13, x.province);
+                            pstmt.setString(14, x.city);
+                            pstmt.setString(15, x.day);
+                            pstmt.setString(16, x.hour);
+                            pstmt.setLong(17, System.currentTimeMillis());
+                        },
+                JdbcExecutionOptions.builder().withBatchSize(50).withBatchIntervalMs(4000).build(),
+                new JdbcConnectionOptions().JdbcConnectionOptionsBuilder()
+                        .withUrl("jdbc:clickhous://hadoop000:8123/pk")
+                        .withDriverName("ru.yandex.clickhouse.ClickHouseDriver")
+                ));;
+        FlinkUtils.env.execute();
+    }
 }
---
+++ imooc-flink-project/src/main/java/com/imooc/flink/domain/Access.java
@@ -0,0 +1,4 @@
+package com.imooc.flink.domain;
+
+public class Access {
+}
