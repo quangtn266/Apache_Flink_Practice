52225bba75170298bd5eda7f45a540660bc35864
quangtn266 <quangtrandn93@gmail.com>
2024-02-07 20:57:11 +0700

    update source code for imooc-flink-datastream/source & sink

--- imooc-flink-datastream/src/main/java/com/imooc/flink/source/StudentSource.java
+++ imooc-flink-datastream/src/main/java/com/imooc/flink/source/StudentSource.java
@@ -1,11 +1,13 @@
 package com.imooc.flink.source;
 
 import com.imooc.flink.utils.MySQLUtils;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
-import javax.security.auth.login.Configuration;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class StudentSource extends RichSourceFunction<Student> {
 
@@ -15,9 +17,27 @@ public class StudentSource extends RichSourceFunction<Student> {
     @Override
     public void open(Configuration parameters) throws Exception {
         connection = MySQLUtils.getConnection();
-        psmt = connection.prepareStatement("select * from student")
+        psmt = connection.prepareStatement("select * from student");
     }
 
     @Override
-    public void close()
+    public void close() throws Exception{
+        MySQLUtils.close(connection, psmt);
+    }
+
+    @Override
+    public void run(SourceContext<Student> ctx) throws Exception {
+        ResultSet rs = psmt.executeQuery();
+        while(rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            ctx.collect(new Student(id, name, age));
+        }
+    }
+
+    @Override
+    public void cancel() {
+
+    }
 }
